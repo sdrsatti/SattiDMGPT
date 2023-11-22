a8a2e54b2036f6d79bdaa7c9d64591762a8ce513
sdrsatti <sdrsatti@gmail.com>
2023-11-22 04:46:22 -0500

    Main

---
+++ README.md
@@ -0,0 +1,15 @@
+Satti DM GPT
+
+A application to test utility of extending OpenAI's ChatGPT to medical use, specifically for Diabetes.
+
+Enclosed are the following Diabetes Guidelines:
+
+Standards of Care in Diabetes—2023 
+
+Author: S. D. Satti, MD
+
+"# Diabetes"
+"# Satti DM GPT" 
+"# Diabetes GPT" 
+"# Endocrinology Bot" 
+"# SattiDMGPT" 
---
+++ SattiDMGPT.py
@@ -0,0 +1,70 @@
+# ''' 
+# This program is uploaded to git and then pushed to https://sattiafibchatgpt.streamlit.app
+# After this program is saved, run from cmd from this directory:
+#     git commit -a -m "Main"
+#     git push
+# This is will update git and then automatically update streamlit.
+# '''
+
+
+from openai import OpenAI
+import streamlit as st
+import time
+
+
+client = OpenAI()
+
+assistant = "asst_2X2nmWYsG2q1r9T7cW8feh17"
+
+thread = "thread_HEoGxA3MMmDgceFKu3vvYkQT"
+
+def getanswer(question):
+
+  message = client.beta.threads.messages.create(
+      thread_id=thread,
+      role="user",
+      file_ids=['file-GFSQ4TYFjfsoEFi3mwhWMsAL'],
+      content= question
+  )
+
+  run = client.beta.threads.runs.create(
+    thread_id=thread,
+    assistant_id=assistant,
+    instructions="Give complete and through answers using the files uploaded and only answer medical questions"
+  )
+
+  while run.status != "completed":
+      time.sleep(1)
+      run = client.beta.threads.runs.retrieve(
+      thread_id=thread,
+      run_id=run.id
+  )
+
+  messages = client.beta.threads.messages.list(
+    thread_id=thread
+  )
+
+  answer = messages.data[0].content[0].text.value
+
+  return(answer)
+
+
+st.set_page_config(page_title='Satti Diabetes GPT', page_icon=':robot')
+
+
+st.header('Satti DM GPT')
+st.write('by S. D. Satti, MD, FACC, FHRS - me@sattimd.com')
+st.write("This is an extension of OpenAI's ChatGPT with additional training using Standards of Care in Diabetes—2023 Guidelines.")
+st.write('')
+
+base_prompt = "Give specific answers. In a separate paragraph, at the end give an itemized list the individual references for the following: "
+input_prompt = st.text_area(label='What is your query:', key='user_input')
+
+prompt = input_prompt
+
+if st.button(label='Submit'):
+    if input_prompt != '':
+        st.write("Thinking...")
+        answer = getanswer(prompt)
+        st.write(answer)
+
